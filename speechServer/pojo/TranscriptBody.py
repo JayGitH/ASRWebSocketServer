3be1324a1924046adf5406439351ae2cff6fa336
lovemefan <450489712@qq.com>
2021-06-09 19:40:45 +0800

    add task id

--- speechServer/pojo/TranscriptBody.py
+++ speechServer/pojo/TranscriptBody.py
@@ -12,33 +12,34 @@ class TranscriptBody:
     """The transcript body is the result of speech translation task.
     Using as the return format from speech SDK (service layer) to speechRoute (routes layer)
     """
-    def __init__(self, task_id: int = None, result: str = None, speech_type: str = None, speech_id: str = None):
+    def __init__(self, task_id: int = None, result: str = None, status: str = None, speech_id: str = None):
         """initial the Transcript body
         Args:
             speech_id (str): status code, detail see backend/utils/StatusCode.py
             result (str)： list of Sentences corresponding to speech,
-            speech_type (str): [final,partial,error,end]
+            status (str): [final,partial,error,end]
             each sentence conclude text and timestamp information.
         """
-        self.task_id = task_id
+        self.__task_id = task_id
 
         if speech_id == 'auto':
-            self.speech_id = f"yuntrans-{IdWorker().get_id()}"
+            self.__speech_id = f"yuntrans-{IdWorker().get_id()}"
         else:
-            self.speech_id = speech_id
+            self.__speech_id = speech_id
 
-        self.result = result
-        self.speech_type = speech_type
+        self.__result = result
+        self.__status = status
 
     def __dict__(self):
         return {
-            "task_id": self.task_id,
-            "speech_id": self.speech_id,
-            "type": self.speech_type,
-            "result": self.result
+            "task_id": self.__task_id,
+            "speech_id": self.__speech_id,
+            "status": self.__status,
+            "result": self.__result
         }
 
     def json(self):
         return json.dumps(self.__dict__())
+
 
 
